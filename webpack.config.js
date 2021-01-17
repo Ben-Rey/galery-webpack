1ac3663abf11a69e5f991271d786b82a953e51c5
Ben Rey <benjamin@morio.co>
2021-01-17 17:09:33 +0100

    test

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry:['./src/index.js'],
     output: {
         filename: 'bundle.js',

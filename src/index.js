c7791cf461179d08042f9e39335a418a13546f2c
Saïd VELO <said.velo@ynov.com>
2021-01-21 13:28:43 +0100

    add json fake db + route to get all favourites

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ const path = require("path");
 
 const app = express();
 
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('./db.json')
+const db = low(adapter)
+
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ images: [], count: 0 })
+    .write()
+
 // Set static path
 app.use(express.static(path.join(__dirname, "client")));
 
@@ -13,7 +23,6 @@ app.use(bodyParser.json());
 const publicVapidKey = "BJUZgBWz1ctYaCXtxs8ks2TgFfR9ehswDHDjS-kIRQ4suyy247IOHJ8skbFZLtZNIreJUevpyvi9p4QYFag-MpU";
 const privateVapidKey = "7WoAeESZo53iNxhQKMl9gRAJ-DxLmKtuUy6HYGgHJ7Q";
 
-const favoriteList = require('./favorites.json')
 
 webpush.setVapidDetails(
     "mailto:test@test.com",
@@ -23,20 +32,34 @@ webpush.setVapidDetails(
 
 // Subscribe Route
 app.post("/subscribe", (req, res) => {
+    // Add a post
+    db.get('images')
+        .push({ title: req.body.title, url: req.body.url })
+        .write()
+    // Increment count
+    db.update('count', n => n + 1)
+        .write()
+
+    // Create payload
+    const payload = 'The image is saved in My Favourites';
 
     // Get pushSubscription object
     const subscription = req.body;
 
     // Send 201 - resource created
-    res.status(201).json({});
-
-    // Create payload
-    const payload = JSON.stringify({ title: "Push Test" });
+    res.status(201).json("OK");
 
     // Pass object into sendNotification
     webpush
         .sendNotification(subscription, payload)
         .catch(err => console.error(err));
+});
+
+app.get("/favourites", (req, res) => {
+    let myFavourites = db.get('images').value();
+    if (myFavourites) {
+        res.status(201).json(myFavourites);
+    }
 });
 
 const port = 5000;

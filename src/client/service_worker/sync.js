c8948043e5835754c73ab1761553bf6481e1289f
Saïd VELO <said.velo@ynov.com>
2021-01-28 15:32:03 +0100

    background sync

--- src/client/service_worker/sync.js
+++ src/client/service_worker/sync.js
@@ -1,6 +1,15 @@
 
-self.addEventListener('sync', function(event) {
-    if (event.tag == 'syncfav') {
-      event.waitUntil(prout());
-    }
-  });
+self.addEventListener('sync', function (event) {
+  if (event.tag == 'syncfav') {
+    event.waitUntil(syncfav());
+  }
+});
+
+function syncfav() {
+  console.log('PROUT')
+  return update({ url: `http://localhost:5000/subscribe` })
+    .then(refresh)
+    .then((favs) => self.registration.showNotification(
+      `${favs.length} attendees to the PWA Workshop`
+    ))
+}
